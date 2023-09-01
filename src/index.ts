2f5de317789fed5074578bf384360370fa2a8915
cuingskot76 <afrizapamujo@gmail.com>
2023-09-01 07:15:30 +0700

    setup initial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World");
+import express from "express";
+import http from "http";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => console.log("Server is running on port 8080"));
