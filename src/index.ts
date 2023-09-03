0dc51d0c101dcc7e9b57429a397747a302d95f09
cuingskot76 <afrizapamujo@gmail.com>
2023-09-03 10:33:23 +0700

    [chore]: add cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 import UserRoute from "../routes/UserRoute";
 
@@ -13,7 +14,7 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
-
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use("/api/v1", UserRoute);
